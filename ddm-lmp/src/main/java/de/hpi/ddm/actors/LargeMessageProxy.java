a2a095a789e7400ab88488249ab2f274bbb4eb79
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2019-11-20 12:56:52 +0100

    Use Akka streams for sending large messages

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,26 +1,29 @@
 package de.hpi.ddm.actors;
 
+import akka.NotUsed;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
-import akka.io.DirectByteBufferPool;
-import akka.serialization.ByteBufferSerializer;
 import akka.serialization.Serialization;
-import akka.serialization.SerializerWithStringManifest;
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.ByteBufferInput;
-import com.esotericsoftware.kryo.io.ByteBufferOutput;
-import com.twitter.chill.SerDeState;
+import akka.serialization.SerializationExtension;
+import akka.serialization.Serializers;
+import akka.stream.ActorMaterializer;
+import akka.stream.SourceRef;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.stream.javadsl.StreamRefs;
 import de.hpi.ddm.structures.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ArrayUtils;
 
-import java.io.IOException;
-import java.io.NotSerializableException;
 import java.io.Serializable;
-import java.nio.ByteBuffer;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
 
@@ -46,77 +49,12 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	}
 
 	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class BytesMessage<T> implements Serializable {
+	public static class BytesMessage implements Serializable {
 		private static final long serialVersionUID = 4057807743872319842L;
-		private T bytes;
-		private String senderIdentifier;
-		private String receiverIdentifier;
-	}
-
-	@NoArgsConstructor
-	public static class LargeMessageByteBufSerializer extends SerializerWithStringManifest implements ByteBufferSerializer {
-		// https://doc.akka.io/docs/akka/2.4/scala/remoting-artery.html#ByteBuffer_based_serialization
-
-		DirectByteBufferPool pool = new akka.io.DirectByteBufferPool( 1024 * 1024, 10);
-
-		@Override
-		public int identifier() {
-			return 1337;
-		}
-
-		@Override
-		public String manifest(Object o) {
-			return "serialized-" + o.getClass().getSimpleName();
-		}
-
-		@Override
-		public byte[] toBinary(Object o) {
-			//ByteBuffer buf = pool.acquire();
-			final ByteBuffer buf = ByteBuffer.allocate(1024);
-
-			try {
-				toBinary(o, buf);
-				// flip() makes a buffer ready for a new sequence of channel-write or relative get operations:
-				// It sets the limit to the current position and then sets the position to zero.
-				buf.flip();
-				// Remaining: return the number of elements between the current position and the limit.
-				final byte[] bytes = new byte[buf.remaining()];
-				buf.get(bytes);
-				return bytes;
-			} finally {
-				pool.release(buf);
-			}
-		}
-
-		@Override
-		public Object fromBinary(byte[] bytes, String manifest) throws NotSerializableException {
-			return fromBinary(ByteBuffer.wrap(bytes), manifest);
-		}
-
-		@Override
-		public void toBinary(Object o, ByteBuffer buf) {
-			ByteBufferOutput output = new ByteBufferOutput(buf, 1024*1024);
-			Kryo kryo = new Kryo();
-			try {
-				kryo.writeClassAndObject(output, o);
-				output.flush();
-            } finally {
-				output.release();
-			}
-		}
-
-		@Override
-		public Object fromBinary(ByteBuffer buf, String manifest) throws NotSerializableException {
-			SerDeState kryo = KryoPoolSingleton.get().borrow();
-
-			try {
-				ByteBufferInput input = new ByteBufferInput(buf);
-				kryo.setInput(input);
-				return kryo.readClassAndObject();
-			} finally {
-				KryoPoolSingleton.get().release(kryo);
-			}
-		}
+		private  SourceRef<Byte> bytes;
+		private  Class klass;
+		private  ActorRef sender;
+		private  ActorRef receiver;
 	}
 
 	/////////////////
@@ -130,7 +68,10 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Behavior //
 	////////////////////
-	
+
+	private final Serialization serialization = SerializationExtension.get(this.context().system());
+	private final ActorMaterializer mat = ActorMaterializer.create(this.context().system());
+
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
@@ -141,30 +82,32 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	}
 
 	private void handle(LargeMessage<?> message) {
-		ActorRef receiver = message.getReceiver();
-		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
+		// Configure proxy actors
+		ActorSelection receiverProxy = this.context().actorSelection(message.receiver.path().child(DEFAULT_NAME));
 
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
+		// Serialization
+		Byte[] bytes = ArrayUtils.toObject(this.serialization.serialize(message.message).get());
 
-		// We need to use actor identifiers as string as they need to be serialized with Kryo. See:
-		// https://doc.akka.io/docs/akka/2.5.3/java/serialization.html#serializing-actorrefs
-        String senderActorIdentifier = Serialization.serializedActorPath(this.sender());
-		String receiverActorIdentifier = Serialization.serializedActorPath(message.getReceiver());
+		// Configure data stream with serialized data
+		final Source<Byte, NotUsed> messageSource = Source.from(Arrays.asList(bytes));
+		final CompletionStage<SourceRef<Byte>> messageRefs = messageSource.runWith(StreamRefs.sourceRef(), this.mat);
 
-		receiverProxy.tell(new BytesMessage<>(message.getMessage(), senderActorIdentifier, receiverActorIdentifier), this.self());
+		// Serialize and stream to other proxy actor
+        messageRefs.thenApply(ref -> new BytesMessage(ref, message.getClass(), this.sender(), message.receiver) )
+				.thenAccept(result -> receiverProxy.tell(result, this.self()));
 	}
 
-	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
+	private void handle(BytesMessage message) {
+		// Collect the bytes with Sink.seq and move them into a list to be serialized
+		final SourceRef<Byte> sourceRef = message.bytes;
+		final Source<Byte, NotUsed> source = sourceRef.getSource();
+		final CompletionStage<List<Byte>> bytesCompletion = source.runWith(Sink.seq(), mat);
 
-		ActorRef sender = this.context().system().provider().resolveActorRef(message.getSenderIdentifier());
-		ActorRef receiver = this.context().system().provider().resolveActorRef(message.getReceiverIdentifier());
-
-		receiver.tell(message.getBytes(), sender);
+		// Deserialize and forward to receiver
+        bytesCompletion.thenAccept(result -> {
+            byte[] bytes = ArrayUtils.toPrimitive(result.toArray(new Byte[result.size()]));
+            Object object = this.serialization.deserialize(bytes, message.klass);
+            message.receiver.tell(object, message.sender);
+        });
 	}
 }
